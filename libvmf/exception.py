6a089845b2385bb755b364c44fec7da68a344634
Dylan Grafmyre <thorsummoner@live.com>
2015-02-01 14:31:58 -0800

    Docstrings

--- libvmf/exception.py
+++ libvmf/exception.py
@@ -1,9 +1,17 @@
+"""Exception classes
+"""
 
 class ValveException(Exception):
+    """Base Exception
+    """
     pass
 
 class ValveKeyError(ValveException):
+    """KeyError specific to map parsing
+    """
     pass
 
 class ValveTypeError(ValveException):
+    """TypeError specific to map parsing
+    """
     pass
